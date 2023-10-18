c86e8dd858a38897af3028ef2e2252f06aebabb2
TDitS <temdit01@yandex.ru>
2023-10-18 20:00:52 +0300

    update test

--- test/test_PrintStudents100.py
+++ test/test_PrintStudents100.py
@@ -54,32 +54,6 @@ class TestPrintStudents100:
 
         return data, students
 
-    @pytest.fixture()
-    def input_data100_2(self) -> tuple[DataType, str]:
-        data: DataType = {
-            "Абрамов Петр Сергеевич":
-                [
-                    ("математика", 100),
-                    ("русский язык", 100),
-                    ("программирование", 100)
-                ],
-
-            "Петров Игорь Владимирович":
-                [
-                    ("математика", 100),
-                    ("русский язык", 100),
-                    ("программирование", 100),
-                    ("литература", 100)
-                ]
-        }
-
-        students: str = ["У студента Абрамов Петр Сергеевич"
-                         " 100 баллов по всем дисциплинам",
-                         "У студента Петров Игорь Владимирович"
-                         " 100 баллов по всем дисциплинам"]
-
-        return data, students
-
     def test_init_print_students_No100(self, input_dataNo100:
                                        tuple[DataType, str]) -> None:
 
@@ -102,14 +76,3 @@ class TestPrintStudents100:
         result100 = PrintStudents100(input_data100[0]).calc()
         assert result100 == input_data100[1]
 
-    def test_init_print_students_100_2(self, input_data100_2:
-                                       tuple[DataType, str]) -> None:
-
-        print_students_100_2 = PrintStudents100(input_data100_2[0])
-        assert input_data100_2[0] == print_students_100_2.data
-
-    def test_print100_2(self, input_data100_2: tuple[DataType, str]) -> None:
-
-        result100 = PrintStudents100(input_data100_2[0]).calc()
-        assert (result100 == input_data100_2[1][0]) | (result100 ==
-                                                       input_data100_2[1][1])
